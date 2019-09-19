1a2cb5dab91bd788f27426dc54a39282a07d66de
AntoineUGOLINI <antoine.ugolini92@orange.fr>
2019-09-19 17:02:38 +0200

    Début de film

---
+++ ProjetFilm/ModelObjet/Film.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ModelObjet
+{
+    public class Film
+    {
+        public int NbEntrees { get; set; }
+        public string PhotoFilm { get; set; }
+        public string TitreFilm { get; set; }
+    }
+}
---
+++ ProjetFilm/ModelObjet/Réalisateur.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModelObjet
+{
+    class Réalisateur
+    {
+        public string NomRealisateur { get; set; }
+        public string PhotoRealisateur { get; set; }
+        public string PrenomRealisateur { get; set; }
+    }
+}
